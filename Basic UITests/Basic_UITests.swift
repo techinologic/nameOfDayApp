7e870d301cc64d0d6548dc964133e97fdebc7b45
Paolo T. Inocencion <Paolo@paolos-mbp.fios-router.home>
2017-06-06 09:28:08 -0400

    Initial Commit

---
+++ Basic UITests/Basic_UITests.swift
@@ -0,0 +1,36 @@
+//
+//  Basic_UITests.swift
+//  Basic UITests
+//
+//  Created by Paolo T. Inocencion on 6/6/17.
+//  Copyright © 2017 Paolo T. Inocencion. All rights reserved.
+//
+
+import XCTest
+@testable import Basic_UI
+
+class Basic_UITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
