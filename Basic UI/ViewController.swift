975420d2dd0577bd1ce19592b59c600ec66add5a
Paolo T. Inocencion <Paolo@paolos-mbp.fios-router.home>
2017-06-06 20:44:04 -0400

    First app

--- Basic UI/ViewController.swift
+++ Basic UI/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var picker: UIDatePicker!
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pickerDidChange(picker)
+    }
+    @IBAction func pickerDidChange(_ sender: Any) {
+        let date:Date = picker.date
+        let formatter:DateFormatter = DateFormatter()
+        formatter.dateFormat = "EEEE"
+        let dayOfWeek:String = formatter.string(from: date)
+        label.text = "The day is a \(dayOfWeek)"
+        
     }
 
     override func didReceiveMemoryWarning() {
